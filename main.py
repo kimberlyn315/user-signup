462e9236360e3b655300c55e86ecef3852f6e500
kimberlyn315 <thompsonkim1590@gmail.com>
2019-04-13 18:35:29 -0500

    Finish user-signup

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-    <form action="action_page.php" style="border:1px solid #ccc">
-        <div class="container">
-            <h1>User Sign Up</h1>
-            <p>Please fill in this form to create an account.</p>
-            <hr>
+<form action="action_page.php" style="border:1px solid #ccc">
+     <div class="container">
+        <h1>User Sign Up</h1>
+         <p>Please fill in this form to create an account.</p>
+        <hr>
 
-            <label>Username: <input type="text" name="username"/></label>
-            <input type="text" placeholder="Enter Username" name="username" required>
-            <label style="color:red">{username_message}</label>
+        <label>Username: <input type="text" name="username"/></label>
+        <input type="text" placeholder="Enter Username" name="username" required>
+        <label style="color:red">{username_message}</label>
            
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
-            <label style="color:red">{password_message}</label>
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+        <label style="color:red">{password_message}</label>
             
-            <label for="psw-repeat"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Repeat Password" name="psw-repeat"
-            required>
+        <label for="psw-repeat"><b>Repeat Password</b></label>
+        <input type="password" placeholder="Repeat Password" name="psw-repeat"
+        required>
 
-            <label>
-                <input type="checkbox" checked="checked" name="remember" style="margin-
-                bottom:15px"> Remember me
-            </label>
+        <label>
+            <input type="checkbox" checked="checked" name="remember" style="margin-
+            bottom:15px"> Remember me
+        </label>
 
             
 
