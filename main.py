2053e7f11cd0e31ddb62ab968661f74de433150e
kimberlyn315 <thompsonkim1590@gmail.com>
2019-04-18 18:14:09 -0500

    Finish user-signup

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
      
-     <div class="container">
-        
+    <div class="container">
         <h1>User Sign Up</h1>
-         
+    </div>
+    
     <div class="container">
         <form action="/action_page.php">
         <label for="usrname">Username</label>
@@ -16,24 +16,16 @@
         </form>
     </div>
 
-    <div id="message">
-        <h3>Password must contain the following:</h3>
-        <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
-        <p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
-        <p id="number" class="invalid">A <b>number</b></p>
-        <p id="length" class="invalid">Minimum <b>3 characters</b></p>
-    </div> 
-
-        <label>
-            <input type="checkbox" checked="checked" name="remember" style="margin-
-            bottom:15px"> Remember me
-        </label>
+    <label>
+        <input type="checkbox" checked="checked" name="remember" style="margin-
+        bottom:15px"> Remember me
+    </label>
        
-        <div class="clearfix">
-            <button type="button" class="cancelbtn">Cancel</button>
-            <button type="submit" class="signupbtn">Sign Up</button>
-        </div>
+    <div class="clearfix">
+        <button type="button" class="cancelbtn">Cancel</button>
+        <button type="submit" class="signupbtn">Sign Up</button>
     </div>
+
 
  
 
