6e0be82df880585e6831e8da8aa9676b7b2039b0
kimberlyn315 <thompsonkim1590@gmail.com>
2019-04-18 18:07:16 -0500

    Finish user-signup

--- main.py
+++ main.py
@@ -3,21 +3,26 @@
      <div class="container">
         
         <h1>User Sign Up</h1>
-         <p>Please fill in this form to create an account.</p>
-        <hr>
+         
+    <div class="container">
+        <form action="/action_page.php">
+        <label for="usrname">Username</label>
+        <input type="text" id="usrname" name="usrname" required>
 
-        <label>Username: <input type="text" name="username"/></label>
-        <input type="text" placeholder="Enter Username" name="username" required>
-        <label style="color:red">{username_message}</label>
-           
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
-        <label style="color:red">{password_message}</label>
-            
-        <label for="psw-repeat"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat"
-        required>
-        <label style="color:red">{password_message}</label>
+        <label for="psw">Password</label>
+        <input type="password" id="psw" name="psw" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{3,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 3 or more characters" required>
+
+        <input type="submit" value="Submit">
+        </form>
+    </div>
+
+    <div id="message">
+        <h3>Password must contain the following:</h3>
+        <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
+        <p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
+        <p id="number" class="invalid">A <b>number</b></p>
+        <p id="length" class="invalid">Minimum <b>3 characters</b></p>
+    </div> 
 
         <label>
             <input type="checkbox" checked="checked" name="remember" style="margin-
