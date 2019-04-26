1ef9db6580f905e5e83f3dc5f875dff21aa6e424
kimberlyn315 <thompsonkim1590@gmail.com>
2019-04-25 19:35:12 -0500

    Finish user-signup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 app = Flask(__name__)
 app.config['DEBUG'] = True 
 
@@ -11,19 +11,27 @@ def message():
 
 
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 def username():
-    if not valid:
-        return error_message
+    username = request.form['username']
+    username_error_message = ''
+    password = request.form['password']
+    password_error_message = ''
+    if len(username) < 3 or len(username) > 20:
+         username_error_message = 'The username is not correct'
+         return render_template('index.html', username_error_message=username_error_message)
+    
+    
+    elif len(password) < 3 or len(password) > 20:
+        password_error_message = 'The password is not correct'
+        return render_template('index.html', password_error_message=password_error_message)
     else:
-        return welcome_message
+        return redirect ('/welcome?username={0}'.format(username))   
 
-@app.route("/")
-def password():
-    if not valid:
-        return error_message
-    else:
-        return welcome_message
+@app.route("/welcome")
+def welcome_page():
+    username = request.args.get('username')
+    return '<h1>Welcome, {0}!</h1>'.format(username)
 
 if __name__ == '__main__':
     app.run()
